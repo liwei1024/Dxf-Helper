605b4865a3a249fcacdf4c4624841f5cf19b0baa
威 李 <22287006@qq.com>
2018-11-01 16:21:18 +0800

    添加项目文件。

---
+++ Dxf Helper/game_status_0.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "game_status_0.h"
+
+
+game_status_0::game_status_0()
+{
+}
+
+
+game_status_0::~game_status_0()
+{
+}
---
+++ Dxf Helper/game_status_0.h
@@ -0,0 +1,8 @@
+#pragma once
+class game_status_0
+{
+public:
+	game_status_0();
+	~game_status_0();
+};
+
